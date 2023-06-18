44adfd7c3f6264b729ece9aaad17680826362344
Jerscape <jeremydutton5@gmail.com>
2023-06-18 14:22:17 -0400

    after eslint run and errors fixed

--- assertEqual.js
+++ assertEqual.js
@@ -1,24 +1,24 @@
-const assertEqual = function(actual, expected){
-  let message = ""
-  if(actual === expected){
-    message = `Assertion Passed: ${actual} === ${expected}`
+const assertEqual = function(actual, expected) {
+  let message = "";
+  if (actual === expected) {
+    message = `Assertion Passed: ${actual} === ${expected}`;
   } else {
-    message = `Assertion Failed: ${actual} != ${expected}`
+    message = `Assertion Failed: ${actual} != ${expected}`;
   }
 
-  console.log(message)
-}
+  console.log(message);
+};
 
-assertEqual("Lighthouse Labs", "Bootcamp")
-assertEqual(1, 1)
-assertEqual("fuzz", "fuzz")
-assertEqual("Blah", "blah")
-assertEqual(7, 10) 
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("fuzz", "fuzz");
+assertEqual("Blah", "blah");
+assertEqual(7, 10);
 
 /*
 const assertEqual = function(actual, expected){
 //JN remember it could be a string or a number, we may need to validate first and have two seperate evaluation equations
-let errorMsg = "Assertion Failed: " + actual + "! === " + expected  
+let errorMsg = "Assertion Failed: " + actual + "! === " + expected
 console.assert(actual === expected, errorMsg)
 
 //this is catching the true, but ALSO the false....
