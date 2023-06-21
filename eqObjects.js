ad4b13a089aaee62ab2e9d7f10af6ad03bcd4ee2
Jerscape <jeremydutton5@gmail.com>
2023-06-21 16:46:48 -0400

    this is not working, revise to for each

--- eqObjects.js
+++ eqObjects.js
@@ -1,68 +1,51 @@
-//assert equals 
-
-const assertEqual = function(actual, expected) {
-  let message = "";
-  if (actual === expected) {
-    message = `Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    message = `Assertion Failed: ${actual} != ${expected}`;
-  }
-
-  console.log(message);
-};
-
-
+//asked us to copy and paste our assert equals function but I don't see the point so I deleted it
 //eqObject
 
-const eqObjects = function(object1, object2){
-  
-  //so basicaly my approach will be to create 4 arrays. 
-  //1 array for each objects keys (2 total)
-  //1 array for each objects values (2 total)
-
-  //first I will then compare length. If they aren't equal, it will immediately return false
-  //then if they are equal, I will assess each key by index against eachother
-  //if any do not match, it will immediatley return false
-  //then if they match, I will compare each arrays keys by index against eachother
-  //if any don't match, it will immediatley return false
-  //otherwise if all the values match, it will return, as at this point, the length, keys, and values ALL match
+const eqObjects = function(object1, object2) {
 
   let object1KeysArr = Object.keys(object1)
   let object2KeysArr = Object.keys(object2)
-  let object1ValuesArr = Object.values(object1)
-  let object2ValuesArr = Object.values(object2)
+  //let object1ValuesArr = Object.values(object1)
+  //let object2ValuesArr = Object.values(object2)
 
   //test key array length
-  if(object1KeysArr.length === object2KeysArr.length){
+  if(object1KeysArr.length === object2KeysArr.length) {
     //if the length matches, compare keys?
     for(let i = 0; i < object1KeysArr.length; i++){
-      if(object1KeysArr[1] !== object2KeysArr[i]){
-        return false
+
+      if(object1KeysArr[i] === object2KeysArr[i]){
+        //even key matches, then assess value
+        //object1KeysArr[i] etc should evaluate to the key name in object 1...which should then pull the value right?
+        if(object1[object1KeysArr[i]] !== object2[object2KeysArr[i]] ) {
+          //if any value doesnt' match return false
+          return false 
+
+        } else{
+          //if each key, and each value match
+          return true
+        }
 
       } else {
-        //if the keys index to index matches, compare value to value
-        for (let j = 0; j < object2ValuesArr.length; j++) {
-          if (object1ValuesArr[j] !== object2ValuesArr[i]) {
-            return false
-
-          } else {
-            return true
-          }
-
-        }//end last for loop
-
-      }// end length else
-
-    }// end initial (outer) for loop
+        //if keys do not match reutrn fasle
+        return false
+      }
+       
+    }
 
   } else {
     //return false immediately if the length of each objects keys array doesn't match
     return false 
-  }
+  } //end length if 
 
-};
-
-
-
+  } // end function
 
 //testing
+
+let testingObject1 = {name: "Juniper", age: "20", occupation: "Student"}
+let testingObject2 = {name: "Juniper", age: "20", occupation: "Student"}
+let testingObject3 = {name: "Jeeves", age: "30", occupation: "Mouse Chaser"}
+let testingObject4 = {name: "Jeeves", age: "30"}
+
+
+eqObjects(testingObject1, testingObject2)
+
