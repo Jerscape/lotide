96a33e9a1e91f91010c62fde8556c6668d922335
Jerscape <jeremydutton5@gmail.com>
2023-06-21 15:07:51 -0400

    first commit of eqObjects

---
+++ eqObjects.js
@@ -0,0 +1,23 @@
+//assert equals 
+
+const assertEqual = function(actual, expected) {
+  let message = "";
+  if (actual === expected) {
+    message = `Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    message = `Assertion Failed: ${actual} != ${expected}`;
+  }
+
+  console.log(message);
+};
+
+//eqObject
+
+const eqObjects = function(object1, object2){
+  
+}
+
+
+
+
+//testing
