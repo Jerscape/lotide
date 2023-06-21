e0f58293be6686bf89858984c17212b515a10418
Jerscape <jeremydutton5@gmail.com>
2023-06-21 11:11:24 -0400

    linting complete

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,35 +1,23 @@
 //takes in an object, AND a value. Returns the first key which contains the given value
-//if no key is found it should return undefined. 
+//if no key is found it should return undefined.
 
-const findKeyByValue = function(object, value){
-  let result = ""
+const findKeyByValue = function(object, value) {
+  let result = "";
   //console.log(convertedToArray)
 
-  for(let item in object){
-    let kValue = object[item]
+  for (let item in object) {
+    let kValue = object[item];
     //console.log("Genre: " + item + "value: " + kValue)
-    if(value === kValue){
+    if (value === kValue) {
       //console.log(item)
-      result = item
-      return result
+      result = item;
+      return result;
     }
   }
 
 };
 
 // testing
-
-const testObject = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"};
-
-//console.log(findKeyByValue(testObject, "The Wire"))
-//console.log(findKeyByValue(testObject, "The Expanse"))
-//findKeyByValue(testObject, "The Wire")
-//findKeyByValue(testObject, "The Expanse")
-
-//assertEqual for tsting
 
 const assertEqual = function(actual, expected) {
   let message = "";
@@ -42,8 +30,23 @@ const assertEqual = function(actual, expected) {
   console.log(message);
 };
 
-//test cases 
+//testing object
+const testObject = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"};
 
-let testCase1 = findKeyByValue(testObject, "The Wire")
+//console.log(findKeyByValue(testObject, "The Wire"))
+//console.log(findKeyByValue(testObject, "The Expanse"))
+//findKeyByValue(testObject, "The Wire")
+//findKeyByValue(testObject, "The Expanse")
 
-assertEqual(testCase1, "drama")
+//assertEqual for tsting
+
+
+
+//test cases
+
+let testCase1 = findKeyByValue(testObject, "The Wire");
+
+assertEqual(testCase1, "drama");
