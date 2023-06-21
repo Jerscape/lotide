3a74a98acd3f069fd2670f165a00a9bb877d88fe
Jerscape <jeremydutton5@gmail.com>
2023-06-21 15:53:07 -0400

    starting anew and referencing my assertArraysEqual function as a model

--- eqObjects.js
+++ eqObjects.js
@@ -11,73 +11,20 @@ const assertEqual = function(actual, expected) {
   console.log(message);
 };
 
-//eq array
-
-function assertArraysEqual(array1, array2) {
-  let result = "";
-  //judge length and if length is the same test each element against each element
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      //assess if any of the values DO NOT match, if so return false immediately
-      if (array1[i] !== array2[i]) {
-        result = false;
-      } else {
-        result = true;
-      }
-    }
-
-  } else {
-    //returns false if the array lengths do not match
-    result = false;
-  }
 
 //eqObject
 
 const eqObjects = function(object1, object2){
   let result = ""
   let object1Keys = Object.keys(object1)
-  let Object2Keys = Object.keys(object2) 
+  let object2Keys = Object.keys(object2)
+  let object1Values = Object.values(object1)
+  let object2Values = Object.values(object2)
+
   console.log(object1Keys)
- 
-  //don't forget to test for length
 
 
-
-  /*let Object1Array = []
-  let Object2Array = []
-
-  for(let item in object1){
-    let kValue1 = object1[item]
-    object1.push([item, kValue1])//push array of key value pair onto array
-  }
-
-  for(let item in object2){
-    let kValue2 = object1[item]
-    object2.push([item, kValue2])//push array of key value pair onto array
-  }
-*/
-
-
-
-/*
-  for(let item in object1){
-    let kValue1 = object1[item]
-    for(item in object2) {
-      let kValue2
-
-      let keyEqual = assertEqual(object1[item], item)
-      let valueEqual 
-
-      if(){
-
-      }
-
-    }
-
-  }*/
-
-
-}
+};
 
 
 
