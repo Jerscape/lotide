97be2adc2b5df56a78b4a03c16c9494e0a4d3db2
Jerscape <jeremydutton5@gmail.com>
2023-06-24 15:30:05 -0400

    revised after speaking with mentor Paola and clarifying the iinstructions. It now performs a call to eqArrays v.s containing the logic directly within in with in (see previous version

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,31 +1,45 @@
 function eqArrays(array1, array2) {
-  let result = "";
-  //judge length and if length is the same test each element against each element
+
+  //judge length
+  //if length is the same test each element against each element
   if (array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++) {
       //assess if any of the values DO NOT match, if so return false immediately
       if (array1[i] !== array2[i]) {
-        result = false;
-      } else {
-        result = true;
+        return false;
       }
     }
 
   } else {
     //returns false if the array lengths do not match
-    result = false;
+    return false;
   }
-  
+
+  return true;
+}
+
+function assertArraysEqual(array1, array2) {
+  //let result = "";
+  //judge length and if length is the same test each element against each element
+  let result = eqArrays(array1, array2)
   console.log(result);
 }
 
 //testing
-console.log("test case 1: ");
-eqArrays([1, 2, 3], [1, 2, 3]);
+/* console.log("test case 1: ");
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
 console.log("test case 2: ");
-eqArrays([4, 5], [5, 6, 9]);
+assertArraysEqual([4, 5], [5, 6, 9]);
 console.log("test case 3: ");
-eqArrays([1, 2, 3],[1, 4, 7]);
+assertArraysEqual([1, 2, 3],[1, 4, 7]);
 console.log("test case 4: ");
-eqArrays(["Fuzz", "Buzz"], ["Fuzz", "Buzz"]);
+assertArraysEqual(["Fuzz", "Buzz"], ["Fuzz", "Buzz"]);
+*/
+
+//testing multi dim arrays
+//console.log("Multi dim test case")
+//assertArraysEqual([["Speed", 200], ["value", 100]], [["Speed", 200], ["value", 100]] )
+
+const animals = ["dog", "Cat", "mouse", "bird"]
+assertArraysEqual(animals, ["dog", "Cat", "mouse", "bird"])
 
