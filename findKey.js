a17b2c5a5acae15b78e74b1c7fa8bb978be9797a
Jerscape <jeremydutton5@gmail.com>
2023-06-25 17:21:24 -0400

    submitting after speaking with Mentor Shaun Purslow, I was't aaccessing the second object key/value correctly, so he had me re-do the guts of my function

--- findKey.js
+++ findKey.js
@@ -11,41 +11,30 @@ const assertEqual = function(actual, expected) {
 
 //find key function
 
-const findKey = function(object, callback){
+const findKey = function(object, callback) {
 
-  for(let item in object){
-    let kValue = object[item]
-
-
+  for (let key in object) {
+    let target = object[key]
+    //console.log(target)
+    let outcome = callback(target)
+    if(outcome === true){
+      return key
     }
-    if(typeof(kValue) === "object"){
-          //if the value is an ojbect (which it is in the testing example)
-      for(let elem in kValue){
-        let innerKValue = kValue[elem]
-        let outcome = callback(kValue[innerKValue])
+    
 
-        if(outcome === true){
-        return innerKValue
-        }0
-      } //end for
+    
+  }//end first for of
 
-    //if not a object of object values
-    } else {
-      
-    }
-
-  }
+}; //end function
 
 
-
-
-
-//testing 
-findKey({
+//testing
+const bloop = findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
+  "Ora":       { stars: 4 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 3) // => "noma"
+}, x => x.stars === 45); // => "noma"
+console.log(bloop)
