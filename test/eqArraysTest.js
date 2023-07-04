c7deeff059f58791b13595bb463898cfec433508
Jerscape <jeremydutton5@gmail.com>
2023-07-04 17:09:11 -0400

    first commit eqArraysTest

---
+++ test/eqArraysTest.js
@@ -0,0 +1,9 @@
+const eqArrays = require("../eqArrays");
+const assertEqual = require("../assertEqual");
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); //should assess true
+console.log(eqArrays([4, 5], [5, 6, 9])); //should assess false
+console.log(eqArrays([1, 2, 3],[1, 4, 7])); //should assess false
+console.log(eqArrays(["Fuzz", "Buzz"], ["Fuzz", "Buzz"])); //should assess true
+
+
