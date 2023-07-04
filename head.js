7e75e3c4a238ba433520249c803dd0916a78d03f
Jerscape <jeremydutton5@gmail.com>
2023-07-04 16:28:02 -0400

    updated with tests removed to new folder and files

--- head.js
+++ head.js
@@ -1,20 +1,10 @@
-const assertEqual = function(actual, expected) {
-  let message = "";
-  if (actual === expected) {
-    message = `Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    message = `Assertion Failed: ${actual} != ${expected}`;
-  }
-
-  console.log(message);
-};
+const assertEqual = require('./assertEqual')
 
 function head(array) {
   let head = array[0];
   return head;
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([7]), 7);
-assertEqual(head([], undefined));
+
+
+module.exports = head
