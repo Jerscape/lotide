463120a2227853c6221fc2a2fbe7ff39fd713514
Jerscape <jeremydutton5@gmail.com>
2023-07-04 16:27:32 -0400

    updated with tests removed to new folder and files

--- tail.js
+++ tail.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let message = "";
-  if (actual === expected) {
-    message = `Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    message = `Assertion Failed: ${actual} != ${expected}`;
-  }
-
-  console.log(message);
-};
 
 const tail = function(array) {
   let result = [];
@@ -22,14 +12,8 @@ const tail = function(array) {
   return result;
 };
 
-const testResult = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(testResult.length, 2);
-assertEqual(testResult[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(testResult[1], "Labs"); // ensure second element is "Labs"
-console.log("=== test result 2 ===");
-const testResult2 = tail([5, 6, 8, 10], [6, 8, 10]);
-assertEqual(testResult2.length, 3);
-assertEqual(testResult2[0], 6);
-assertEqual(testResult2[1], 8);
-assertEqual(testResult2[2], 10);
+//exporting tail function
+module.exports = tail
+
+
 
