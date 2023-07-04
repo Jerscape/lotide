06899ac481b93710798a8fab27f9055361a20eb7
Jerscape <jeremydutton5@gmail.com>
2023-07-04 17:10:40 -0400

    testing removed and added to eqArraysTest folder and file

--- eqArrays.js
+++ eqArrays.js
@@ -33,8 +33,5 @@ function eqArrays(array1, array2) {
   return true;
 }
 
-//test cases
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); //should assess true
-console.log(eqArrays([4, 5], [5, 6, 9])); //should assess false
-console.log(eqArrays([1, 2, 3],[1, 4, 7])); //should assess false
-console.log(eqArrays(["Fuzz", "Buzz"], ["Fuzz", "Buzz"])); //should assess true
+module.exports = eqArrays
+
