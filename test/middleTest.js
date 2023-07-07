f89da08b16a8b5133225209fb51f0f2e939f2e0f
Jerscape <jeremydutton5@gmail.com>
2023-07-07 15:06:42 -0400

    committing middle test

--- test/middleTest.js
+++ test/middleTest.js
@@ -6,41 +6,25 @@ const eqArrays = require('../eqArrays')
 //bryan gomes - use the eqArrays
 
 const result1 = middle(["Blur", "Oasis", "Pulp", "Radiohead", "James"])
+const result2 = middle(['Blur','Pulp','Radiohead', 'James'])
 
 describe("#middle", () => {
   it("returns Pulp for ['Blur','Oasis', 'Pulp', 'Radiohead', 'James']", () => {
-    //assert.deepEqual(assertArraysEqual(result1, ['Pulp']), true)
     assert.deepEqual(eqArrays(result1, ['Pulp']), true)
-    //note assertArraysEqual takes in two parameters, ou are only passing one
-    //but at this moment, I cannot get my head into how this will work as a tst 
+ 
   })
 
-  // it("returns 10 for [5, 10, 15]", () => {
-  //   assert.deepEqual(assertArraysEqual([5, 10, 15]), [10])
-  // })
+  it("returns Pulp and Radiohead for ['Blur','Pulp','Radiohead', 'James']", () => {
+    assert.deepEqual(eqArrays(result2, ['Pulp', 'Radiohead']), true)
+ 
+  })
 
-  // it("returns ['Oasis', Radiohead]", () => {
-  //   assert.deepEqual(assertArraysEqual(["Blur", "Oasis", "Radiohead", "James"]), ["Oasis", "Radiohead"])
-  // })
+  it("returns Pulp and Radiohead for ['Blur','Pulp','Radiohead', 'James']", () => {
+    assert.deepEqual(eqArrays(result1, ['Pulp', 'Blur', 'Radiohead']), false)
+ 
+  })
 
 })
-
-//TEST WAS WORKING WITH JUST MIDDLE 
-
-// describe("#middle", () => {
-//   it("returns Pulp for ['Blur','Oasis', 'Pulp', 'Radiohead', 'James']", () => {
-//     assert.deepEqual(middle(["Blur", "Oasis", "Pulp", "Radiohead", "James"]), ["Pulp"])
-//   })
-
-//   it("returns 10 for [5, 10, 15]", () => {
-//     assert.deepEqual(middle([5, 10, 15]), [10])
-//   })
-
-//   it("returns ['Oasis', Radiohead]", () => {
-//     assert.deepEqual(middle(["Blur", "Oasis", "Radiohead", "James"]), ["Oasis", "Radiohead"])
-//   })
-
-// })
 
 
 
