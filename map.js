c0c46051a1a4608a41bbc9706c761929cb9eba51
Jerscape <jeremydutton5@gmail.com>
2023-07-07 17:19:05 -0400

    map linted

--- map.js
+++ map.js
@@ -1,51 +1,33 @@
 
 //need to refactor my assertArraysEqual then copy it here
 
-const map = function(array, callback){
+const map = function(array, callback) {
   let results = [];
 
-  for(let item of array){
-    results.push(callback(item))
+  for (let item of array) {
+    results.push(callback(item));
   }
 
   
   return results;
 
-}
+};
 
 // testing
 
-function assertArraysEqual(array1, array2) {
-  let result = "";
-  //judge length and if length is the same test each element against each element
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      //assess if any of the values DO NOT match, if so return false immediately
-      if (array1[i] !== array2[i]) {
-        result = false;
-      } else {
-        result = true;
-      }
-    }
 
-  } else {
-    //returns false if the array lengths do not match
-    result = false;
-  }
-  
-  console.log(result);
-}
 
+const eqArrays = require("./eqArrays");
 
 const words = ["ground", "control", "to", "major", "tom"];
-//const results1 = map(words, word => word = word[0])
-//console.log(results1)
 
 //const testArray = ['g', 'c', 't' ,'m', 't' ]
-const bloop = map(words, word => word = "bloop")
-const bloopTest = ['bloop', 'bloop', 'bloop', 'bloop', 'bloop']
+const bloop = map(words, word => word = "bloop");
+const bloopTest = ['bloop', 'bloop', 'bloop', 'bloop', 'bloop'];
 
-//assertArraysEqual(results1, testArray)
-//console.log("bloop test is: " + assertArraysEqual(bloop, bloopTest))
-const bloopResult = assertArraysEqual(bloop, bloopTest)
-console.log("bloop result: " , bloopResult)
+//as assertArraysEqual console logs and does not return, this test does not work
+//instead I am going to use eqArrays
+//const bloopResult = assertArraysEqual(bloop, bloopTest)
+//console.log("bloop result: " , bloopResult)
+let bloopResult = eqArrays(bloop, bloopTest);
+console.log(bloopResult);
