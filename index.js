c099c89d3203530a941b4e90d13f48cd9e36ebef
Jerscape <jeremydutton5@gmail.com>
2023-07-08 12:26:42 -0400

    committing exports partially done

---
+++ index.js
@@ -0,0 +1,25 @@
+
+//assertions and testing
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const eqArrays = require('./eqArrays');
+const eqObects = require('./eqObjects');
+
+//regular
+
+const countLetters = require('./countLetters')
+const countOnly = require('./countOnly')
+const findKey = require('./findKey')
+
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+
+
+//export 
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle
+};
