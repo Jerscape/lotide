ed56e4a1ac4a0d22701f3bc13c25b15744c3fe50
Jerscape <jeremydutton5@gmail.com>
2023-07-08 12:36:00 -0400

    all utilities exported

--- index.js
+++ index.js
@@ -11,10 +11,16 @@ const eqObects = require('./eqObjects');
 const countLetters = require('./countLetters')
 const countOnly = require('./countOnly')
 const findKey = require('./findKey')
-
+const findKeyByValue = require('./findKeyByValue')
+const flatten = require('./flatten')
 const head = require('./head');
-const tail = require('./tail');
+const letterPositions = require('./letterPositions')
+const map = require('./map')
 const middle = require('./middle');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil')
+const without = require('./without')
+
 
 
 //export 
