d9a2dc92297edceef9b1cd415e8f774726e5ad7c
Jerscape <jeremydutton5@gmail.com>
2023-07-10 21:04:13 -0400

    updated head with changes based on mentor feedback

--- head.js
+++ head.js
@@ -1,11 +1,13 @@
+
+//imports
 const assertEqual = require('./assertEqual');
 
-
+//head function, identifies first element in array
 const head = function(array) {
   let head = array[0];
   return head;
 };
 
 
-
+//returns new array with first elment of submitted array
 module.exports = head;
