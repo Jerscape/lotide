068ae7a537020be3abc5029de9c55df9ad8c9744
Jerscape <jeremydutton5@gmail.com>
2023-07-10 22:37:31 -0400

    adding changes to countLetters.js based on mentor feedback

--- countLetters.js
+++ countLetters.js
@@ -1,20 +1,21 @@
 
+
+//finds all letters in input array, and returns an object with keys representing the letters
+//and their respective values representing their total count in the sentence
 const countLetters = function(sentence) {
   let lettersCount = {};
   sentence = sentence.toLowerCase();
 
   for (const elem of sentence) {
 
-    //screen out blanks
     if (elem !== " ") {
-      //assess if already in the object
+
       if (!lettersCount[elem]) {
         
         lettersCount[elem] = 1;
   
       } else {
-        //add to existing
-        // don't forget to count
+        
         lettersCount[elem] += 1;
         
       }
@@ -26,27 +27,6 @@ const countLetters = function(sentence) {
 
 };
 
-//THIS DOES NOT WORK AND MENTOR CONFIRMED IT WILL NOT WORK, BUT THE INSTRUCTIONS SAY TO PASTE IT IN HERE?
-//ASSERT EQUALS
-const assertEqual = function(actual, expected) {
-  let message = "";
-  if (actual === expected) {
-    message = `Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    message = `Assertion Failed: ${actual} != ${expected}`;
-  }
-
-  console.log(message);
-};
-
 //export function
-
 module.exports = countLetters;
 
-//put in tests here
-
-//console.log(countLetters("My yellow beetle"));
-//let result = countLetters("My yellow beetle");
-//console.log(result)
-//assertEqual(result, { m: 1, y: 2, e: 4, l: 3, o: 1, w: 1, b: 1, t: 1 });
-//console.log(countLetters("My name is Jeremy"));
