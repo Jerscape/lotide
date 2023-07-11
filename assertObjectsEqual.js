1c51fd16cc0b146e99f2b276c73f665b5df1d0ca
Jerscape <jeremydutton5@gmail.com>
2023-07-10 22:32:35 -0400

    adding updates to objects equal based on mentor feedback

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,22 +1,12 @@
+//Imports
 const eqObjects = require('./eqObjects');
 
-
-//EQ assertObjectsEqual
-
+//assertObjectsEqual compares two objects for equality and results log of result
 const assertObjectsEqual = function(actual, expected){
   const inspect = require('util').inspect; 
   let result = eqObjects(actual, expected)
   console.log(result)
 }
 
-let testObj1 = {name: "Jeremy", location: "Tbay"}
-let testObj2 = {name: "Jeremy", location: "Tbay"}
-let testObj3 = {name: "Jeremy", location: "MiddleMarch"}
-
-// console.log("should be true")
-// assertObjectsEqual(testObj1, testObj2)
-// console.log("should be false")
-// assertObjectsEqual(testObj1, testObj3)
-
-
+//Exports
 module.exports = assertObjectsEqual 
