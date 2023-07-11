d92155416653fdde4f47bbfbc90020eb01353a77
Jerscape <jeremydutton5@gmail.com>
2023-07-10 22:29:32 -0400

    commiting after adding updates based on mentor feedback

--- takeUntil.js
+++ takeUntil.js
@@ -1,7 +1,6 @@
 
-
-const fivesData = [5, 10, 15, 20, 25, 30, 35, 40, 45];
-
+//take until accets array,and callback function. It returns a slice at the front of the array
+//stopping at the first truthy value returned by the callback
 const takeUntil = function(array, callback) {
   let result = [];
 
@@ -15,9 +14,11 @@ const takeUntil = function(array, callback) {
 
 };
 
-
+//testing left here to demonstrate use of callback to evaluate truthiness
+//const fivesData = [5, 10, 15, 20, 25, 30, 35, 40, 45];
 //const result = takeUntil(fivesData, item => item === 10) //x => x <= will return true when x hits or equals 25 so your call will be takeUntil(fivesData, true)
 // const result = takeUntil(fivesData, x => x === 15);
 // console.log(result);
 
+//export function
 module.exports = takeUntil
