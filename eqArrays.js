a6548a420e5febbcb5c1424c50e1de5bad58d9c6
Jerscape <jeremydutton5@gmail.com>
2023-07-10 22:24:41 -0400

    commiting updates based on recommendation from mentor

--- eqArrays.js
+++ eqArrays.js
@@ -1,25 +1,11 @@
-//note the instructions were to paste our previous assertEqual function into this
-//but I ended up writing a totally different solution that doesn't use it
-//so it's presence here is redundant and should probably be removed, but I kept it because of the instructions....
-/*
-const assertEqual = function(actual, expected) {
-  let message = "";
-  if (actual === expected) {
-    message = `Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    message = `Assertion Failed: ${actual} != ${expected}`;
-  }
-
-  console.log(message);
-};*/
 
 
 let eqArrays = function(array1, array2) {
 
-  //judge length
   //if length is the same test each element against each element
   if (array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++) {
+
       //assess if any of the values DO NOT match, if so return false immediately
       if (array1[i] !== array2[i]) {
         return false;
@@ -31,8 +17,10 @@ let eqArrays = function(array1, array2) {
     return false;
   }
 
+  //returns true if array length matches and values of both arrays match
   return true;
 };
 
+//exports
 module.exports = eqArrays;
 
